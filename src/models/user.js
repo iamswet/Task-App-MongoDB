7e49ca2f943a43d0a7a0233a22ce721730f8057f
iamswet <isitswet@gmail.com>
2024-07-17 01:22:41 +0530

    a

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
-const jwt=require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const userschema = mongoose.Schema({
   name: {
@@ -26,44 +26,55 @@ const userschema = mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    unique:true,
-    required:true,
+    unique: true,
+    required: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error("Not a valid Email");
       }
     },
   },
-  tokens:[{
-    token:{
-      type:String,
-      required:true
-    }
-  }]
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
-userschema.methods.generateAuthToken=async function(){
-  const user=this
-  const token=jwt.sign({_id:user._id.toString()},'finishbysunday')
-  
-  user.tokens=user.tokens.concat({token})
-  await user.save()
+//userschema.methods.userPublicProfile= function(){             toJSON is used to return what we want hence we decide what data to send. instead of setting userPublicProfile, we simply use to JSON
+userschema.methods.toJSON = async function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
+};
 
-  return token
-}
+userschema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "finishbysunday");
 
-userschema.statics.findByCredentials=async(email,password)=>{
-  const user=await User.findOne({email})
-  if(!user){
-    throw new Error("Invalid input")
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+userschema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Invalid input");
   }
-  const isMatch=bcrypt.compare(password,user.password)
-  if(!isMatch){
-    throw new Error("Invalid input")
+  const isMatch = bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid input");
   }
 
-  return user
-}
+  return user;
+};
 
 //converting text password to bcrypt hash password
 userschema.pre("save", async function (next) {
@@ -72,7 +83,7 @@ userschema.pre("save", async function (next) {
     user.password = await bcrypt.hash(user.password, 8);
   }
 
-  next()
+  next();
 });
 
 const User = mongoose.model("User", userschema);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,28 +1,29 @@
 const express = require("express");
-const User=require('../models/user')
-const auth=require('../middleware/auth')
+const User = require("../models/user");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
 //only authenticate login
-router.post("/user/login",async(req,res)=>{
-  
-  try{
-    const user = await User.findByCredentials(req.body.email,req.body.password)
-    const token=await user.generateAuthToken()
-    res.status(200).send({user, token})
-  }catch(error){
-    return res.status(400).send(error)
+router.post("/user/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    //    res.status(200).send({user: user.getPublicProfile(), token})       we are using toJSON so no need to get public profile seperately
+    res.status(200).send({ user, token });
+  } catch (error) {
+    return res.status(400).send(error);
   }
-
-})
+});
 
 router.post("/create-user", async (req, res) => {
   const user1 = new User(req.body);
   try {
-    
     await user1.save();
-    const token=await user1.generateAuthToken()
-    res.status(201).send({user1,token});
+    const token = await user1.generateAuthToken();
+    res.status(201).send({ user1, token });
   } catch (error) {
     res.send(400).send(error);
   }
@@ -34,32 +35,32 @@ router.post("/create-user", async (req, res) => {
   //})
 });
 
-router.get('/users/me',auth,async(req,res)=>{
-  res.send(req.user)
-})
+//fetch our own user ID
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
 
-router.post('/users/logout',auth,async(req,res)=>{
-  try{
-    req.user.tokens=req.user.tokens.filter((token)=>{
-      return token.token!==req.token
-    })
-    await req.user.save()
-    res.send()
-  }catch(error){
-    res.status(500).send(error)
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
   }
-})
- 
-router.post('/users/logoutall',auth,async(req,res)=>{
-  try{
-    req.user.tokens=[]
-    await req.user.save()
-    res.send()
-  }catch(error){
-    res.status(500).send(error)
-  }
-})
+});
 
+router.post("/users/logoutall", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 router.get("/fetchall", async (req, res) => {
   try {
@@ -76,6 +77,10 @@ router.get("/fetchall", async (req, res) => {
   //})
 });
 
+/* 
+
+We are removing this so that 1 user can't change fetch the data of other users (navigating to others accounts)
+
 router.get("/fetch/:id", async (req, res) => {
   try {
     const userdata = await User.findById(req.params.id);
@@ -86,54 +91,67 @@ router.get("/fetch/:id", async (req, res) => {
   } catch (error) {
     res.status(400).send(error);
   }
-
-  router.patch("/update-user/:id", async (req, res) => {
-    const updates=Object.keys(req.body)
-    const allowedUpdates=['name','email','password','age']
-    const isValidOperation=updates.every((update)=>allowedUpdates.includes(update))
-    
-    if(!isValidOperation){
-      return res.status(400).send({error:'Invalid request'})
-    }
-
-    try {
-      const user=await User.findById(req.params.id)
-      updates.forEach((update)=>{
-        user[update]=req.body[update]
-      })
-      await user.save()
-
-      //const user = await User.findByIdAndUpdate(req.params.id, req.body, {  new: true, runValidators: true, });
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user).status(200);
-    } catch (error) {
-      res.send(error).status(400);
-    }
   });
+*/
 
-  router.delete("/delete-user/:id", async (req, res) => {
-    try {
-      const userdata = await User.findByIdAndDelete(req.params.id);
-      if (!userdata) {
-        return res.status(404).send();
-      }
-      res.send(userdata);
-    } catch (error) {
-      res.status(400).send(error);
+//update profile
+//router.patch("/update-user/:id", async (req, res) => {
+  router.patch("/users/me", async (req, res) => {
+    const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid request" });
+  }
+
+  try {
+//    const user = await User.findById(req.params.id);
+    updates.forEach((update) => {
+      req.user[update] = req.body[update];
+    });
+    await req.user.save();
+
+    /*  const user = await User.findByIdAndUpdate(req.params.id, req.body, {  new: true, runValidators: true, });
+    if (!user) {
+      return res.status(404).send();
     }
-  });
+    */
 
-  //const id1=req.params.id
-  //User.findById(id1).then((users)=>{
-  //    if(!users){
-  //        res.status(201).send()
-  //    }
-  //    res.send(users)
-  //}).catch((error)=>{
-  //    res.status(400).send(error)
-  //})
+
+    res.send(req.user).status(200);
+  } catch (error) {
+    res.send(error).status(400);
+  }
 });
+
+//  router.delete("/delete-user/:id", async (req, res) => {           specific user should be able to delete only their ID
+router.delete("/users/me", auth, async (req, res) => {
+  try {
+    //    const userdata = await User.findByIdAndDelete(req.params.id);   if we use the delete-users/:id
+    /*
+    const userdata = await User.findByIdAndDelete(req.params.id);
+    if (!userdata) {
+      return res.status(404).send();
+    }
+ */
+    await req.user.remove();
+    res.send(req.user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//const id1=req.params.id
+//User.findById(id1).then((users)=>{
+//    if(!users){
+//        res.status(201).send()
+//    }
+//    res.send(users)
+//}).catch((error)=>{
+//    res.status(400).send(error)
+//})
 
 module.exports = router;
