c1d61ca95cd20e8fb6d276b3c5342768691a9d8e
iamswet <isitswet@gmail.com>
2024-07-19 01:13:44 +0530

    a

--- src/models/task.js
+++ src/models/task.js
@@ -10,6 +10,11 @@ const taskschema = mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  owner:{
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  }
 });
 
 const tasks = mongoose.model("tasks", taskschema);
--- src/routers/task.js
+++ src/routers/task.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const tasks = require("./../models/task");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
-router.post("/create-task", async (req, res) => {
+router.post("/create-task", auth, async (req, res) => {
+  //const task1 = new tasks(req.body);
+  const task1 = new tasks({
+    ...req.body,
+    owner: req.user._id,
+  });
   try {
-    const task1 = new tasks(req.body);
     await task1.save(req.body);
     res.status(201).send(task1);
   } catch (error) {
@@ -21,12 +26,24 @@ router.post("/create-task", async (req, res) => {
   //    });
 });
 
-router.get("/fetchtasks", async (req, res) => {
+router.get("/fetchtasks",auth ,async (req, res) => {
   try {
-    const task1 = await tasks.find();
+    const task1 = await tasks.find({owner:req.user._id});
     res.send(task1);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+router.get("/tasks/:id", auth, async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await tasks.findOne({ _id, owner: req.user._id });
+    if (!task) {
+      return res.status(404).send();
+    }
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
@@ -42,11 +59,11 @@ router.patch("/update-task/:id", async (req, res) => {
   }
 
   try {
-    const taskinconsider=await tasks.findById(req.params.id)
-    updates.forEach((update)=>{
-      taskinconsider[update]=req.body[update]
-    })
-    await taskinconsider.save()
+    const taskinconsider = await tasks.findById(req.params.id);
+    updates.forEach((update) => {
+      taskinconsider[update] = req.body[update];
+    });
+    await taskinconsider.save();
 
     //const task = await tasks.findByIdAndUpdate(req.params.id, req.body, {new: true,runValidators: true,});
     if (!task) {
